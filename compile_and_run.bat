ba42cfc49709ad69b0b0a4ba1133b62ea9fccb55
George Tsiamasiotis <gtsiam@windowslive.com>
2020-11-20 18:08:31 +0200

    Move building to cargo make

--- compile_and_run.bat
+++
@@ -1,2 +0,0 @@
-wasm-pack build --dev --target no-modules frontend-web
-serve .
--- compile_release.bat
+++
@@ -1,3 +0,0 @@
-wasm-pack build --release --target no-modules frontend-web
-cp frontend-web/pkg/frontend_web_bg.wasm assets
-cp frontend-web/pkg/frontend_web.js assets
